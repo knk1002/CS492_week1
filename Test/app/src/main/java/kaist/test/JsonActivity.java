3c3b531629009315ce55c93bfa3f2eea2ffdfcd7
CLANAD <CLANAD>
2015-12-28 20:41:07 +0900

    Implemented displaying rankings from Marumaru.in

--- Test/app/src/main/java/kaist/test/JsonActivity.java
+++ Test/app/src/main/java/kaist/test/JsonActivity.java
@@ -10,10 +10,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.support.v4.app.Fragment;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import java.lang.String;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
+
+import android.widget.ArrayAdapter;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 @SuppressLint("ValidFragment")
 public class JsonActivity extends Fragment {
     Context mContext;
+
+    private ListView m_ListView;
+    private ArrayAdapter<String> m_Adapter;
+    Document doc;
+    ArrayList<String> temp = new ArrayList<String>();
 
     public JsonActivity(Context context)
     {
@@ -23,10 +44,55 @@ public class JsonActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+
         View view = inflater.inflate(R.layout.activity_json, null);
+        // Android에서 제공하는 string 문자열 하나를 출력 가능한 layout으로 어댑터 생성
+        m_Adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1);
+
+        // Xml에서 추가한 ListView 연결
+        m_ListView = (ListView) view.findViewById(R.id.listview);
+
+        // ListView에 어댑터 연결
+        m_ListView.setAdapter(m_Adapter);
+        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+        jsoupAsyncTask.execute();
+
+
+
 
         return view;
     }
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                doc = Jsoup.connect("http://marumaru.in/").userAgent("Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52").get();
+                Elements items = doc.select("td.ranic > a");
+                for (Element item : items) {
+                    temp.add(item.text());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            for(String s : temp) {
+                m_Adapter.add(s);
+            }
+
+        }
+    }
+
 
 
 }
